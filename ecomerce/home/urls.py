a365406fcb433c1dd76c2b3f2ffa07fcde4bf5ff
SHIBINSR <shibinsr17@gmailcom>
2025-01-28 04:36:16 +0530

    working in product details page

--- ecomerce/home/urls.py
+++ ecomerce/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('base',views.base,name="Base"),
     path('',views.home,name="Home"),
-    path("productview",views.productview,name="Productview"),
+    path("product-view",views.productview,name="Productview"),
     path("filter-category",views.filter_category,name="Filter_category"),
+    path("product-details",views.product_details,name="Productdetails")
 ]
--- ecomerce/home/views.py
+++ ecomerce/home/views.py
@@ -25,7 +25,6 @@ def home(request):
     return render(request,"home.html",context)
 
 def productview(request):
-    
     category = Category.objects.all()
     product = Products.objects.all()
     
@@ -38,23 +37,26 @@ def productview(request):
 def filter_category(request):
     product = Products.objects.all()
     is_error = False
-    if request.method == "POST":
-        jsondata = json.loads(request.body.decode("utf-8"))
-        req = jsondata.get("category")
+    if request.method == 'POST':
+        jsondata = json.loads(request.body.decode('utf-8'))
+        req = jsondata.get("categories")
         product = Products.objects.filter(category__id__in=req).distinct()
         if len(req) == 0:
             product = Products.objects.all()
         if len(product) == 0:
             is_error = True
-
         context = {
-            'filtered_products':product,
-            'is_error':is_error,
+            'products':product,
+            "is_error":is_error
         }
-        data = render_to_string('includes/ajax/product_filtered.html', context)
-    return JsonResponse({"status":"success","data":data})
+        data = render_to_string("includes/ajax/product_filtered.html",context)
+
+    return JsonResponse({"status":"success",
+                         "data":data})
           
         
+def product_details(request):
+    return render(request,"product-details.html")
      
      
          
